ec6967622ee790eef2979ab2a08349dfc1c109f6
Jefferson Santos <jeffersonscr12@gmail.com>
2023-01-03 10:26:30 -0300

    path mapping

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,20 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
