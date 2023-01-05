08df9661f347857db12877a61974a283f6db35fe
Jefferson Santos <jeffersonscr12@gmail.com>
2023-01-05 10:39:31 -0300

    finalizando parte de navegacao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { AppRoutes } from './app.routes'
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+import { useTheme } from "styled-components/native";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
-    return(
-        <NavigationContainer>
-            <AppRoutes/>
-        </NavigationContainer>
-    )
+    const { COLORS } = useTheme();
+    return (
+        <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+            <NavigationContainer>
+                <AppRoutes />
+            </NavigationContainer>
+        </View>
+    );
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,12 +5,14 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useState } from "react";
 
 export function NewGroup() {
+    const [group, setGroup] = useState('');
     const { navigate } = useNavigation();
 
     const handleNew = () => {
-        navigate("players", { group: "Rocket" });
+        navigate("players", { group });
     };
 
     return (
@@ -25,7 +27,7 @@ export function NewGroup() {
                     subtitle="crie a turma para adicionar as pessoas"
                 />
 
-                <Input placeholder="Nome da turma" />
+                <Input placeholder="Nome da turma" onChangeText={setGroup}/>
 
                 <Button
                     title="Criar"
