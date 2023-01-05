9cb33f71904f5a71125b930e0544f17953cc2cf0
Jefferson Santos <jeffersonscr12@gmail.com>
2023-01-05 10:11:51 -0300

    implementando navegacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,28 @@
+import { useNavigation } from "@react-navigation/native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-import logoImg from '@assets/logo.png';
+import logoImg from "@assets/logo.png";
 
 type Props = {
-  showBackButton?: boolean;
-}
+    showBackButton?: boolean;
+};
 
 export function Header({ showBackButton = false }: Props) {
-  return (
-    <Container>
-      { 
-        showBackButton &&
-        <BackButton>
-          <BackIcon />
-        </BackButton>
-      }
+    const { navigate } = useNavigation();
 
-      <Logo source={logoImg} />
-    </Container>
-  )
+    const handleGoBack = () => {
+        navigate('groups')
+    }
+
+    return (
+        <Container>
+            {showBackButton && (
+                <BackButton onPress={handleGoBack}>
+                    <BackIcon />
+                </BackButton>
+            )}
+
+            <Logo source={logoImg} />
+        </Container>
+    );
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,27 +7,32 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
-  return (
-    <Container>
-      <Header showBackButton />
+    const { navigate } = useNavigation();
 
-      <Content>
-        <Icon />
-        
-        <Highlight 
-          title="Nova turma"
-          subtitle="crie a turma para adicionar as pessoas"
-        />
+    const handleNew = () => {
+        navigate("players", { group: "Rocket" });
+    };
 
-        <Input
-          placeholder="Nome da turma"
-        />
+    return (
+        <Container>
+            <Header showBackButton />
 
-        <Button 
-          title="Criar"
-          style={{ marginTop: 20 }}
-        />
-      </Content>
-    </Container>
-  )
+            <Content>
+                <Icon />
+
+                <Highlight
+                    title="Nova turma"
+                    subtitle="crie a turma para adicionar as pessoas"
+                />
+
+                <Input placeholder="Nome da turma" />
+
+                <Button
+                    title="Criar"
+                    style={{ marginTop: 20 }}
+                    onPress={handleNew}
+                />
+            </Content>
+        </Container>
+    );
 }
